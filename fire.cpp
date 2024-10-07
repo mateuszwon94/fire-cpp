faa13acd59c8e8868d9feb10f3299f097bfd89d2
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2024-10-07 20:24:42 +0200

    Small refactoring

--- fire.cpp
+++ fire.cpp
@@ -39,35 +39,37 @@ std::pair<size_t, size_t> get_terminal_size() {
 
 class fire {
 public:
-    fire() : _fire(), gen(rd()) {
+    fire() : _fire(), _gen(_rd()) {
         // Initialize with the balnks
         auto [width, height] = get_terminal_size();
-        for (size_t i = 0; i < height; ++i) {
-            _fire.push_back(std::deque<uint8_t>(width, 0));
+        _width = width;
+        _height = height;
+        for (size_t i = 0; i < _height; ++i) {
+            _fire.push_back(std::deque<uint8_t>(_width, 0));
         }
     }
 
     void seed() {
         // Seed the last row with the random characters
-        for (size_t i = 0; i < _fire.back().size(); ++i) {
+        for (size_t i = 0; i < _width; ++i) {
             _fire.back()[random_index()] = random_char();
         }
         // Add some more new blanks to make it look more like a fire
-        for (size_t i = 0; i < _fire.back().size(); ++i) {
+        for (size_t i = 0; i < _width; ++i) {
             _fire.back()[random_index()] = 0;
         }
     }
 
     void calculate() {
         // Calculate new value of the character based on the window to the right
-        for (size_t i = 0; i < _fire.size()-1; ++i) {
-            for (size_t j = 0; j < _fire[i].size()-1; ++j) {
+        for (size_t i = 0; i < _height-1; ++i) {
+            for (size_t j = 0; j < _width-1; ++j) {
                 _fire[i][j] = average(_fire[i][j], _fire[i+1][j], _fire[i][j+1], _fire[i+1][j+1]);
             }
         }
         // then base of the window to the left to give it some smooth (and make it flow upwards instead of left-upwards)
-        for (size_t i = 0; i < _fire.size()-1; ++i) {
-            for (size_t j = _fire[i].size()-1; j > 0; --j) {
+        for (size_t i = 0; i < _height-1; ++i) {
+            for (size_t j = _width-1; j > 0; --j) {
                 _fire[i][j] = average(_fire[i][j], _fire[i+1][j], _fire[i][j-1], _fire[i+1][j-1]);
             }
         }
@@ -75,8 +77,8 @@ public:
 
     void print() {
         // print all but last line
-        for (size_t i = 0; i < _fire.size()-1; ++i) {
-            for (size_t j = 0; j < _fire[i].size()-2; ++j) {
+        for (size_t i = 0; i < _height-1; ++i) {
+            for (size_t j = 0; j < _width-2; ++j) {
                 std::cout << fire_chars[_fire[i][j]];
             }
             std::cout << std::endl;
@@ -87,29 +89,29 @@ public:
         // recalculate the sire of the matrix based on the new terminal values
         auto [width, height] = get_terminal_size();
 
-        auto curr_height = get_current_height();
-        auto curr_width = get_current_width();
-
-        if (height > curr_height) {
-            _fire.push_front(std::deque<uint8_t>(curr_width, 0));
-        } else if (height < curr_height) {
+        if (height > _height) {
+            _fire.push_front(std::deque<uint8_t>(_width, 0));
+        } else if (height < _height) {
             _fire.pop_front();
         }
 
-        if (width > curr_width) {
+        if (width > _width) {
             for (auto& row : _fire) {
                 row.push_front(0);
             }
-        } else if (width < curr_width) {
+        } else if (width < _width) {
             for (auto& row : _fire) {
                 row.pop_front();
             }
         }
+
+        _width = width;
+        _height = height;
     }
 
 private:
     double random() {
-        return std::generate_canonical<double, 10>(gen);
+        return std::generate_canonical<double, 10>(_gen);
     }
 
     uint8_t random_char() {
@@ -120,18 +122,11 @@ private:
         return std::floor(random() * (_fire.back().size() - 1));
     }
 
-    inline size_t get_current_width() {
-        return _fire.front().size();
-    }
-
-    inline size_t get_current_height() {
-        return _fire.size();
-    }
-
     std::deque<std::deque<uint8_t>> _fire;
+    size_t _width, _height;
 
-    std::random_device rd;              // a seed source for the random number engine
-    std::mt19937 gen;                   // mersenne_twister_engine seeded with rd()
+    std::random_device _rd;              // a seed source for the random number engine
+    std::mt19937 _gen;                   // mersenne_twister_engine seeded with _rd()
 
     static constexpr std::string_view fire_chars = std::string_view(" ,;+ltgti!lI?/\\|)(1}{][rcvzjftJUOQocxfXhqwWB8&%$#@");
 };
